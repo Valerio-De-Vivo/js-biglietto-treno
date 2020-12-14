3ace5fb2baba9ec4676ecc2012c1ba294170b60e
Valerio <valeriodevivo90@gmail.com>
2020-12-14 16:50:59 +0100

    Finish with bonus

--- js/script.js
+++ js/script.js
@@ -2,16 +2,29 @@ var km = parseInt(prompt('Quanti km vuoi percorrere?'));
 var eta = parseInt(prompt('Quanti anni hai?'));
 
 
+// MESSAGGIO PROMO ETA
+var promo;
+
+
 // CALCOLO PREZZO BIGLIETTO
 var prezzo = km * 0.21;
+promo = "Nessuno sconto disponibile";
 
 
 // CALCOLO SCONTO BIGLIETTO
 
 if (eta < 18) {
     prezzo -= prezzo * 20 / 100;
+    promo = "Sconto del 20%";
 } else if ( eta > 65) {
     prezzo -= prezzo * 40 / 100;
+    promo= "Sconto del 40%";
 }
 
-console.log(prezzo);
+// PREZZO SENZA DECIMALI
+
+var price = prezzo.toFixed(2)
+
+document.getElementById('price').innerHTML = price + "€";
+
+document.getElementById('promo').innerHTML = promo;
